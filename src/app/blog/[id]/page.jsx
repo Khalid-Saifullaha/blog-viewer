2cb98d881cea00c4a6e8a9949886af420661563f
Your Name <khalidsaifullaha3@gmail.com>
2025-01-24 16:44:35 +0600

    BlogDetails page min hight added

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -3,7 +3,7 @@ export default async function BlogDetails({ params }) {
   const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
   const post = await res.json();
   return (
-    <div className="container mx-auto p-4 ">
+    <div className="container mx-auto p-4 min-h-screen">
       <div className="container mx-auto p-4 ">
         <h1 className="text-2xl font-bold mb-4">{post.title}</h1>
         <p>{post.body}</p>
