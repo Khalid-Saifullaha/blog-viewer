71f82b8fb8f6cf871c935856d58af039abf1e5d4
Your Name <khalidsaifullaha3@gmail.com>
2025-01-24 16:32:47 +0600

    blog card added

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,19 +4,26 @@ export default async function Home() {
 
   return (
     <div className="container mx-auto p-4">
-      <h1 className="text-2xl font-bold mb-4">Blog Posts</h1>
-      <ul className="space-y-4">
-        {posts.slice(0, 20).map((post) => (
-          <li key={post.id}>
+      <h1 className="text-2xl font-bold my-6 ">Blogs</h1>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+        {posts.slice(0, 18).map((post) => (
+          <div
+            key={post.id}
+            className="bg-white shadow-lg rounded-lg p-6 border hover:shadow-xl transition-shadow duration-300"
+          >
+            <h2 className="text-lg font-semibold mb-2 text-gray-800">
+              {post.title}
+            </h2>
+            <p className="text-gray-600">{post.body.substring(0, 100)}... </p>
             <a
               href={`/blog/${post.id}`}
-              className="text-blue-500 hover:underline"
+              className="text-blue-500 hover:underline mt-4 block"
             >
-              {post.title}
+              Read More
             </a>
-          </li>
+          </div>
         ))}
-      </ul>
+      </div>
     </div>
   );
 }
